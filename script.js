7d0ad94a2939a91f754157be4f4e39cc4e3d0e11
Lelio Victor <leliov.vieira@gmail.com>
2022-04-20 17:53:48 -0300

    Construction of second screen: title done

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("all working good");
+function quizzPage () {
+    const id = "2"; //Usarei um único quizz para estilizar tudo e montar o código, depois trocar isso.
+
+
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/${id}`)
+
+    promise.then(renderQuizz);
+    promise.catch(deuErrado);
+}
+
+function renderQuizz(ArrayObject) {
+    const element = document.querySelector(".screen_second");
+
+    //element.innerHTML = ``
+
+}
+
+function deuCerto () {
+    console.log("Deu certo");
+}
+
+function deuErrado(erro) {
+    console.log("Erroooou " + erro.response.status);
+    console.log(erro.response.data);
+}
+
+quizzPage();
